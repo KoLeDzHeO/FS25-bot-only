54df6e77271a5cf6f5a3d5b6a7237efd67914565
KoLeDzHeO <80348407+KoLeDzHeO@users.noreply.github.com>
2025-08-06 02:47:47 +0200

    chore: remove duplicate docstring

--- utils/logger.py
+++ utils/logger.py
@@ -8,3 +8,8 @@ logging.basicConfig(level=logging.INFO, format="%(message)s")
 def log_debug(message: str) -> None:
     """Выводит отладочное сообщение."""
     logging.info(message)
+
+
+def log_info(message: str) -> None:
+    """Выводит информационное сообщение."""
+    logging.info(message)
